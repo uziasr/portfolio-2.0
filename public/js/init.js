a45400b0e1ece0689b26f33e00f858d0276da30b
Uzias Rivera <uziasrivera@gmail.com>
2020-04-09 09:50:40 -0700

    added user feedback when  message sent

--- public/js/init.js
+++ public/js/init.js
@@ -152,7 +152,7 @@
       $.ajax({
 
 	      type: "POST",
-	      url: "inc/sendEmail.php",
+	      url: "https://email-sender-pro.herokuapp.com/",
 	      data: data,
 	      success: function(msg) {
 
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -26,33 +26,47 @@ const Contact = () => {
         subject: "",
         message: ""
     })
-    
-    const changeHandler = e =>{
-        setMessage({...message,
-        [e.target.name]: e.target.value
+
+    const [success, isSuccessful] = useState({
+        submit: false,
+        sent: false,
+    })
+
+    const changeHandler = e => {
+        setMessage({
+            ...message,
+            [e.target.name]: e.target.value
         })
         console.log(message)
     }
-    
+
     const onSubmit = (e) => {
         setErrors({})
         console.log("this is being submitted")
         e.preventDefault()
-        schema.validate(message, {abortEarly:false})
-        .then(re=>{
-            axios.post('https://email-sender-pro.herokuapp.com/', message)
-        })
-        .catch(err=>{
-            setErrors((state) => {
-                const errorObj = {}
-                err.errors.forEach(error=>{
-                    const key = error.split(' ')[0]
-                   errorObj[key] = error 
-                })
-                return {...state, ...errorObj}
+        schema.validate(message, { abortEarly: false })
+            .then(re => {
+                axios.post('https://email-sender-pro.herokuapp.com/', message)
+                    .then(res => {
+                        console.log("this email has been successfully sent", res)
+                        isSuccessful({
+                            submit: true,
+                            sent: true
+                        })
+                    })
+                    .catch(err => console.log("this post has not been sent", err))
             })
-        },)
-    } 
+            .catch(err => {
+                setErrors((state) => {
+                    const errorObj = {}
+                    err.errors.forEach(error => {
+                        const key = error.split(' ')[0]
+                        errorObj[key] = error[0].toUpperCase()+error.slice(1)
+                    })
+                    return { ...state, ...errorObj }
+                })
+            })
+    }
 
     return (
         <section id="contact">
@@ -69,26 +83,26 @@ const Contact = () => {
                         <fieldset>
                             <div>
                                 <label htmlFor="contactName">Name <span className="required">*</span></label>
-                                <input type="text" onChange={e=> changeHandler(e)} name="name" size={35} id="contactName" />
-                                {validationErrors.name ? <p style={{color:errorColor}}>{validationErrors.name}</p> : null}
+                                <input type="text" onChange={e => changeHandler(e)} name="name" size={35} id="contactName" />
+                                {validationErrors.name ? <p style={{ color: errorColor }}>{validationErrors.name}</p> : null}
                             </div>
                             <div>
                                 <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                                <input type="text" onChange={e=> changeHandler(e)} name="email" size={35} id="contactEmail" />
-                                {validationErrors.email ? <p style={{color:errorColor}}>{validationErrors.email}</p> : null}
+                                <input type="text" onChange={e => changeHandler(e)} name="email" size={35} id="contactEmail" />
+                                {validationErrors.email ? <p style={{ color: errorColor }}>{validationErrors.email}</p> : null}
                             </div>
                             <div>
                                 <label htmlFor="contactSubject">Subject</label>
-                                <input type="text" onChange={e=> changeHandler(e)} name="subject" size={35} id="contactSubject" />
-                                {validationErrors.subject ? <p style={{color:errorColor}}>{validationErrors.subject}</p> : null}
+                                <input type="text" onChange={e => changeHandler(e)} name="subject" size={35} id="contactSubject" />
+                                {validationErrors.subject ? <p style={{ color: errorColor }}>{validationErrors.subject}</p> : null}
                             </div>
                             <div>
                                 <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                                <textarea cols={50} onChange={e=> changeHandler(e)} name="message" rows={15} id="contactMessage" defaultValue={""} />
-                                {validationErrors.message ? <p style={{color:errorColor}}>{validationErrors.message}</p> : null}
+                                <textarea cols={50} onChange={e => changeHandler(e)} name="message" rows={15} id="contactMessage" defaultValue={""} />
+                                {validationErrors.message ? <p style={{ color: errorColor }}>{validationErrors.message}</p> : null}
                             </div>
                             <div>
-                                <button className="submit" onClick={e=>onSubmit(e)}>Submit</button>
+                                <button className="submit" onClick={e => onSubmit(e)}>Submit</button>
                                 <span id="image-loader">
                                     <img alt="" src="images/loader.gif" />
                                 </span>
@@ -96,11 +110,15 @@ const Contact = () => {
                         </fieldset>
                     </form> {/* Form End */}
                     {/* contact-warning */}
-                    <div id="message-warning"> Error boy</div>
                     {/* contact-success */}
-                    <div id="message-success">
-                        <i className="fa fa-check" />Your message was sent, thank you!<br />
-                    </div>
+                    {
+                        (success.submit && success.sent) ?
+                            <div style={{ display: "inline-block" }} className="message-success">
+                                <i className="fa fa-check" />Your message was sent, thank you!<br />
+                            </div>
+                            :
+                            <div style={{ display: "inlineBlock" }} id="message-warning"> Error boy</div>
+                    }
                 </div>
                 <aside className="four columns footer-widgets">
                 </aside>
