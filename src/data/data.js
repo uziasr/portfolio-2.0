c3a9c14e78a004909e384f2983ddb75e65186fea
Uzias Rivera <uziasrivera@gmail.com>
2021-03-23 09:39:12 -0700

    updated ecommerce link

--- src/data/data.js
+++ src/data/data.js
@@ -169,7 +169,7 @@ export const projects = [
         description: "Simply designed e-commerce web application. Powered by the powerful Commerce.js",
         img: "images/projects/ecommerceCard.JPG",
         descriptionImg: "images/projects/ecommerce.JPG",
-        src: "https://6057e26cac3debce02809fd7--pensive-mcnulty-3e0eae.netlify.app/",
+        src: "https://ecommerce-office.netlify.app/",
         tags: "React Stripe Commerce.js Material UI",
         gh: "https://github.com/uziasr/ecommerce_site",
     }
